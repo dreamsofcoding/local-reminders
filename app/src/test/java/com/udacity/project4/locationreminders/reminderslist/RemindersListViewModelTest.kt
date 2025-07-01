188b510ac87f5a6ee1991f76342b3db86b4f7678
dreamsofcoding <coding.dreams@pm.me>
2025-07-01 13:29:02 +0100

    finishing the testing implementation

--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,13 +1,78 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth
+import com.udacity.project4.locationreminders.MainCoroutineRule
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
+import kotlin.test.assertEquals
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
 
-    //TODO: provide testing to the RemindersListViewModel and its live data objects
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule(StandardTestDispatcher())
+
+    private var fakeDataTest: FakeDataSource = FakeDataSource()
+
+    private lateinit var viewModel: RemindersListViewModel
+
+    private val dataTest = ReminderDTO(
+        "Reminder 1",
+        "Reminder Description 1",
+        "Brandenburger Tor, Berlin",
+        52.516,
+        13.377
+    )
+
+    @Before
+    fun initConfig() {
+        viewModel = RemindersListViewModel(
+            ApplicationProvider.getApplicationContext(), fakeDataTest
+        )
+        stopKoin()
+    }
+
+    @Test
+    fun testInternalError() = runTest {
+        fakeDataTest.isInternalError = true
+        fakeDataTest.deleteAllReminders()
+        viewModel.loadReminders()
+        assertEquals("Internal errors while getting reminders", viewModel.showSnackBar.value)
+    }
+
+    @Test
+    fun testLoadingData() = runTest {
+        // GIVEN a reminder in the fake data source
+        fakeDataTest.isInternalError = false
+        fakeDataTest.saveReminder(dataTest)
+
+        // WHEN loading reminders
+        viewModel.loadReminders()
+
+        // THEN launching loadReminders(), showLoading should be true
+        Truth.assertThat(viewModel.showLoading.value).isTrue()
+
+        val scheduler = mainCoroutineRule.dispatcher.scheduler
+        scheduler.advanceUntilIdle()
+
+        // THEN after the work completes, showLoading should be false
+        Truth.assertThat(viewModel.showLoading.value).isFalse()
+    }
 
 }
--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,16 +1,77 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import com.google.common.truth.Truth
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
+import com.udacity.project4.locationreminders.data.dto.Result
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class SaveReminderViewModelTest {
 
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
-    //TODO: provide testing to the SaveReminderView and its live data objects
+    private var fakeDataTest: FakeDataSource = FakeDataSource()
 
+    private lateinit var viewModel: SaveReminderViewModel
 
+    private val dataTestValid = ReminderDataItem(
+        "Reminder 1",
+        "Reminder Description 1",
+        "Brandenburger Tor, Berlin",
+        52.516,
+        13.377
+    )
+    private val dataTestInValid = ReminderDataItem(
+        null,
+        "Reminder Description 1",
+        null,
+        52.516,
+        13.377
+    )
+
+    @Before
+    fun initConfig() {
+        viewModel = SaveReminderViewModel(
+            ApplicationProvider.getApplicationContext(), fakeDataTest
+        )
+        stopKoin()
+    }
+
+    @Test
+    fun testSaveReminderInValid() = runTest {
+        viewModel.validateAndSaveReminder(dataTestInValid)
+        val actual = fakeDataTest.getReminder(dataTestInValid.id)
+        Truth.assertThat(actual).isEqualTo(Result.Error("Not found reminder"))
+    }
+
+    @Test
+    fun testSaveReminderSuccess() = runTest {
+        fakeDataTest.deleteAllReminders()
+        viewModel.validateAndSaveReminder(dataTestValid)
+        val actual = fakeDataTest.getReminder(dataTestValid.id)
+        val expected = Result.Success(
+            ReminderDTO(
+                title = dataTestValid.title,
+                description = dataTestValid.description,
+                location = dataTestValid.location,
+                latitude = dataTestValid.latitude,
+                longitude = dataTestValid.longitude,
+                id = dataTestValid.id
+            )
+        )
+        Truth.assertThat(actual).isEqualTo(expected)
+    }
 }
